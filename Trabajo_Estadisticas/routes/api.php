8b07b376d1e7fc956f1a030bc55e98552d099447
gokusent <bereck_ssj@outlook.com>
2025-02-07 19:50:10 +0100

    feat: :sparkles: Recolección de estaciones colectivas e individuales

--- Trabajo_Estadisticas/routes/api.php
+++ Trabajo_Estadisticas/routes/api.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\AemetController;
+use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\almacenaStat;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('/clima', [AemetController::class, 'getWeather']);
-Route::get('/test', function () {
-    return response()->json(['message' => 'API funcionando']);
-});
-
+Route::get('/estaciones', [WeatherController::class, 'getAllStations']);
+Route::get('/estacion/{idema}', [WeatherController::class, 'getWeatherData']);
